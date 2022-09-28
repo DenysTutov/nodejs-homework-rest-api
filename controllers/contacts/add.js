bc570b14f1883b3791b8c0476c333ea0dee9576a
DenysTutov <93935076+DenysTutov@users.noreply.github.com>
2022-09-28 20:20:43 +0300

    Bugfix add

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,11 +1,6 @@
 const { Contact } = require("../../models/contact");
-const { RequestError } = require("../../helpers");
 
 const add = async (req, res, next) => {
-  if (!req.body.name || !req.body.email || !req.body.phone) {
-    throw RequestError(400, "missing fields");
-  }
-
   const result = await Contact.create(req.body);
 
   res.status(201).json({ status: "success", code: 201, data: { result } });
